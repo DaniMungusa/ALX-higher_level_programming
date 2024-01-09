6ac4c93b0ff9279d7a9865820003eb499d1eb42f
danimungusa <dmungusa.dm@gmail.com>
2024-01-09 08:58:08 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def main():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    main()
